89126c0eff9cbb16dfd0853a9e55dcf14df1a149
Malcolm Sharpe <sharpe.malcolm@gmail.com>
2012-08-11 20:13:05 -0400

    Full card move and VP token tracking.

--- pce.js
+++ pce.js
@@ -49,7 +49,7 @@ $(document).ready(function() {var main = function() {
       var outerdata = msg.data;
       var msgname = outerdata.messageName;
       var gmdata = outerdata.data;
-      
+
       if (msgname == 'gameSetup') {
         // Get correspondence between player names and player indices.
         playerNames = [];
@@ -59,7 +59,22 @@ $(document).ready(function() {var main = function() {
 
         console.log('Players: ' + prettyJSON(playerNames));
 
-        // TODO: Maybe initialize starting decks here.
+        // Generate the card IDs for the starting decks, since they aren't
+        // reported to us directly.
+        // TODO: Does this actually work reliably?
+        var nextIndex = {
+          'copper': 0,
+          'estate': 0
+        }
+        $.each(gmdata.startingDecks, function(playerIndex, cards) {
+          var player = playerNames[playerIndex];
+          console.log('Player ' + player + ' starts with deck:');
+          $.each(cards, function(j, card) {
+            var qualifiedCard = card + '.' + nextIndex[card];
+            ++nextIndex[card];
+            console.log('  ' + qualifiedCard);
+          });
+        });
       } else if (msgname == 'moveCards') {
         // Process a card move that is NOT triggered by this player's input.
         $.each(gmdata, function(i, moveData) {
@@ -76,13 +91,40 @@ $(document).ready(function() {var main = function() {
       } else if (msgname == 'uiMultiSelectResponse') {
         if (gmdata.card != undefined) {
           // Process a card move that IS triggered by this player's input.
-          processMove(gmdata.card, gmdata.source, gmdata.destination);
+          var card = gmdata.card;
+          if (card == 'back') {
+            // The only known case where this happens is with Chancellor,
+            // and it corresponds to a fake card move, not an actual one.
+            // So, as long as it is consistent with a Chancellor play, ignore
+            // it.
+            if (gmdata.source.area.name != 'deck'
+                || gmdata.destination.area.name != 'discard') {
+              console.log('ERROR: Unknown moved card for response data: ' + prettyJSON(gmdata));
+            }
+            return;
+          }
+          processMove(card, gmdata.source, gmdata.destination);
         } else if (gmdata.id == 'done' || gmdata.id == 'playAll') {
           // Pressed the "Done" or "Play Treasures" button.
           // The treasure plays come in as moveCards messages, so we don't need
           // to do anything here.
+        } else if (gmdata.id == 'setAside') {
+          // Chose to set aside a card from Native Village.
+          // This is OK, as the move is reported separately.
+          // Not sure whether Library uses this, but anyway it's okay because the
+          // cards remain in globalReveal.
+        } else if (gmdata.id == 'take') {
+          // Chose to take cards off the Native Village mat.
+          // TODO: Is this triggered by any other card?
         } else {
           console.log('ERROR: Unrecognized uiMultiSelectResponse: ' + prettyJSON(gmdata));
+        }
+      } else if (msgname == 'updateState') {
+        // This is only necessary to track VP tokens. Sometimes an updateState
+        // message will not report VP tokens, and that is okay.
+        var vptokens = gmdata.victoryPointTokens;
+        if (vptokens != undefined) {
+          setVPTokens(vptokens.playerIndex, vptokens.numVictoryPointTokens);
         }
       }
     }
@@ -101,7 +143,11 @@ $(document).ready(function() {var main = function() {
     else if (data.name == 'hand') return 'hand (' + player + ')';
     else if (data.name == 'deck') return 'deck (' + player + ')';
     else if (data.name == 'globalReveal') return 'globalReveal';
+    else if (data.name == 'globalRevealTwo') return 'globalRevealTwo';
     else if (data.name == 'reveal') return 'reveal (' + player + ')';
+    else if (data.name == 'nativeVillageMat') return 'nativeVillageMat (' + player + ')';
+    else if (data.name == 'islandMat') return 'islandMat (' + player + ')';
+    else if (data.name == 'durationMat') return 'durationMat (' + player + ')';
     else {
       console.log('ERROR: Unknown area: ' + prettyJSON(data));
       return 'ERROR';
@@ -110,6 +156,11 @@ $(document).ready(function() {var main = function() {
 
   function processMove(card, src, dst) {
     console.log('Move ' + card + ' from ' + prettyArea(src) + ' to ' + prettyArea(dst));
+  }
+
+  function setVPTokens(playerIndex, numVPTokens) {
+    var player = playerNames[playerIndex];
+    console.log('Player ' + player + ' has ' + numVPTokens + ' VP tokens');
   }
 }
 
@@ -123,4 +174,4 @@ var runInPageContext = function(fn) {
 
 runInPageContext(main);
 
-})
+});
